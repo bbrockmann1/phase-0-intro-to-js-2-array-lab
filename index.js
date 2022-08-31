809679f354ab8f1349eb6abb49bee094ec4f9ea9
Robert Brockmann <bbrockmann1@gmail.com>
2022-08-30 18:50:53 -0600

    commit tree e3bb74a6fda5d1b9cf2504a91853cb4775d4f0a9

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+const destructivelyAppendCat = () => {
+    cats.push('Ralph');
+};
+
+const destructivelyPrependCat = () => {
+    cats.unshift('Bob');
+};
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop();
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift();
+}
+
+const appendCat = (name) => {
+    return [...cats, name];
+}
+
+const prependCat = (name) => {
+    return [name, ...cats]
+}
+
+const removeLastCat = () => {
+    return cats.slice(0, cats.length - 1);
+}
+
+const removeFirstCat = () => {
+    return cats.slice(1);
+}
